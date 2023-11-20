d0cc0cf0229bc15de083a4c16ea48084ccc5a0f7
Dhanveer Ramnauth <dhanr1606@gmail.com>
2023-11-20 17:13:36 +1100

    init

---
+++ build/CMakeFiles/Chelton.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chelton.dir/src/main.cpp.o"
+  "CMakeFiles/Chelton.dir/src/main.cpp.o.d"
+  "Chelton"
+  "Chelton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chelton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
